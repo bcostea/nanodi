c6997d64f8119000661b7fe676b667af005aad4b
Bogdan Costea <bogdan@costea.us>
2009-11-03 16:53:43 +0000

    Testing tab indentation

--- NanoDI/State.cs
+++ NanoDI/State.cs
@@ -5,11 +5,11 @@ using System.Text;
 
 namespace NanoDI
 {
-    public enum State
-    {
-        None = 0,
-        Initializing = 1,
-        Initialized = 2,
-        Destroyed = 3
-    }
+	public enum State
+	{
+		None = 0,
+		Initializing = 1,
+		Initialized = 2,
+		Destroyed = 3
+	}
 }
