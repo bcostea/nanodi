c8772f58005980472433effa297ec00c94ecdc4b
Bogdan Costea <bogdan@devaccelerator.ro>
2016-10-06 10:38:52 +0300

    generated and uploaded nuget package

--- Ndi/Properties/AssemblyInfo.cs
+++ Ndi/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NanoDI")]
-[assembly: AssemblyDescription("Dependency Injection Container")]
+[assembly: AssemblyTitle("Ndi")]
+[assembly: AssemblyDescription("A lean and mean DI Container")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("GridPulse")]
-[assembly: AssemblyProduct("NanoDI")]
-[assembly: AssemblyCopyright("Copyright © GridPulse 2009")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ndi")]
+[assembly: AssemblyCopyright("Copyright © Bogdan Costea 2009-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0.0")]
-[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
